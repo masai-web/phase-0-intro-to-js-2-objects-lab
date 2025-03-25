6c017a1994ed1fdbc787abd1e01f17aecddf8a6a
+ <masaitaboi@gmail.com>
2025-03-26 00:14:49 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee={
+    name:"masai",
+    streetAddress:"policeline"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     return {...employee,[key]:value,
+     }
+
+       
+    }
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+employee[key]=value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name){
+const newEmployee={...employee};
+delete newEmployee[name]
+return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
